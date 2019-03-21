f41e241d66939b3fe3cc6c710d27dc979659e150
Metin Dumandağ <metindumandag@gmail.com>
2019-03-21 12:53:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import simpy
 import random
+import numpy as np
 
 PATIENCE_MEAN = 60
 INTER_ARRIVAL_TIME_MEAN = 14.3
@@ -33,7 +34,7 @@ class Customer:
                                                                 FRONT_DESK_OPERATOR_SERVICE_TIME_STD))
             print(f'{self.name} finished talking to the front desk operator at {self.environment.now}')
 
-        with self.expert_operator.request() as request:
+        with self.expert_operator.request(0) as request:
             patience = random.expovariate(1 / PATIENCE_MEAN)
             results = yield request | self.environment.timeout(patience)
 
@@ -48,13 +49,29 @@ class Customer:
 
 
 def generate_customers(environment, front_desk_operator, expert_operator):
-    for i in range(10):
+    for i in range(5):
         Customer(f'Customer {i+1}', environment, front_desk_operator, expert_operator)
         yield environment.timeout(random.expovariate(1 / INTER_ARRIVAL_TIME_MEAN))
 
 
+def generate_expert_breaks(environment, expert_operator, p):
+    while p.is_alive:
+        next_break_time = np.random.poisson(60)
+        yield environment.timeout(next_break_time)
+
+        with expert_operator.request(1) as request:
+            print(f'break requested at {environment.now} {next_break_time}')
+            yield request
+
+            print(f'break started at {environment.now}')
+            yield environment.timeout(3)
+            print(f'break finished at {environment.now}')
+
+
+
 environment = simpy.Environment()
 front_desk_operator = simpy.Resource(environment, capacity=1)
-expert_operator = simpy.Resource(environment, capacity=1)
-environment.process(generate_customers(environment, front_desk_operator, expert_operator))
+expert_operator = simpy.PriorityResource(environment, capacity=1)
+p = environment.process(generate_customers(environment, front_desk_operator, expert_operator))
+environment.process(generate_expert_breaks(environment, expert_operator, p))
 environment.run()
