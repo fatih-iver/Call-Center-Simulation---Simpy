28283a123008ad6de6dc1267a4d7b7f15b02e294
Fatih İver <fatih.iver@boun.edu.tr>
2019-03-31 18:48:32 +0300

    Update call-center.py

--- call-center.py
+++ call-center.py
@@ -1,26 +1,89 @@
 import random
-import numpy as np
+import numpy
+import simpy
 
-callNumber = 10
+RANDOM_SEED = 978
 
-interArrivalTimeMean = 14.3 # Exponential (min)
+INTERARRIVAL_RATE = 14.3
+MEAN_RENEGE =  60
 
-recordingTimeMean = 7.2 # Normal (min)
+MEAN_FRONT_DESK = 7.2
+STD_FRONT_DEKS = 2.7
 
-recordingTimeSTD = 2.7 # Normal (min)
+MEAN_EXPERT= 10.2
 
-serviceTimeMean = 10.2 # Exponential (min)
+SERVICE_RANGE = [50, 90]
+random.seed(RANDOM_SEED)
 
-numberOfBreaksMean = 8 # Poisson
+service_times_front_desk = [] 
+service_times_expert = []
 
-interArrivalTimes = [random.expovariate(interArrivalTimeMean) for _ in range(callNumber)]
+queue_w_times_front_desk = []
+queue_w_times_expert = []
 
-recordingTimes = [random.normalvariate(recordingTimeMean, recordingTimeSTD) for _ in range(callNumber)]
+class Customer(object):
+    def __init__(self, name, env, front_desk_operator, expert_operator):
+        self.env = env
+        self.name = name
+        self.arrival_to_front_desk = None
+        self.arrival_to_expert = None
+        self.action = env.process(self.call())
+    
+    def call(self):
+        print('%s initiated a call at %g' % (self.name, self.env.now))
 
-serviceTimes = [random.expovariate(serviceTimeMean) for _ in range(callNumber)]
+        self.arrival_to_front_desk = self.env.now
 
-takeBreak = random.choice([True, False])
+        with front_desk_operator.request() as req:
 
-numberOfBreaks = np.random.poisson(numberOfBreaksMean)
+            patience = random.expovariate(MEAN_RENEGE)
 
-print(numberOfBreaks)
+            results = yield req | env.timeout(patience)
+
+            if req in results: 
+                print('%s is assigned to the front desk operator at %g' % (self.name, self.env.now))
+                queue_w_times_front_desk.append(self.env.now - self.arrival_to_front_desk)
+                yield self.env.process(self.get_front_desk_service())
+                print('%s is done with front desk operator at %g' % (self.name, self.env.now))
+            else:
+                print('%s is reneged from the queue of the front desk operator at %g' % (self.name, self.env.now))
+                env.exit()
+
+        self.arrival_to_expert = self.env.now
+
+        with expert_operator.request() as req:
+            
+            patience = random.expovariate(MEAN_RENEGE)
+
+            results = yield req | env.timeout(patience)
+
+            if req in results: 
+                print('%s is assigned to the expert operator at %g' % (self.name, self.env.now))
+                queue_w_times_expert.append(self.env.now - self.arrival_to_expert)
+                yield self.env.process(self.get_expert_service())
+                print('%s is done with expert operator at %g' % (self.name, self.env.now))
+            else:
+                print('%s is reneged from the queue of the expert operator at %g' % (self.name, self.env.now))
+                env.exit()
+
+    def get_front_desk_service(self):
+        #duration = numpy.random.lognormal(MEAN_FRONT_DESK, STD_FRONT_DEKS)
+        duration = random.expovariate(MEAN_EXPERT)
+        yield self.env.timeout(duration)
+        service_times_front_desk.append(duration)
+
+    def get_expert_service(self):
+        duration = random.expovariate(MEAN_EXPERT)
+        yield self.env.timeout(duration)
+        service_times_expert.append(duration)
+
+def customer_generator(env, front_desk_operator, expert_operator):
+    for i in range(10):
+        yield env.timeout(random.expovariate(INTERARRIVAL_RATE))
+        Customer('Cust %s' % (i+1), env, front_desk_operator, expert_operator)  
+
+env = simpy.Environment()
+front_desk_operator = simpy.Resource(env, capacity = 1)
+expert_operator = simpy.Resource(env, capacity = 1)
+env.process(customer_generator(env, front_desk_operator, expert_operator))
+env.run() 
