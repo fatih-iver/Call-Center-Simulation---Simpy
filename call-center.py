7b7da5da38c8df5141199b351b2f99bb15d393c7
Fatih İver <fatih.iver@boun.edu.tr>
2019-03-31 21:00:04 +0300

    Update call-center.py

--- call-center.py
+++ call-center.py
@@ -3,7 +3,6 @@ import numpy
 import simpy
 
 RANDOM_SEED = 978
-random.seed(RANDOM_SEED)
 
 INTERARRIVAL_RATE = 14.3
 MEAN_RENEGE =  60
@@ -13,21 +12,40 @@ STD_FRONT_DEKS = 2.7
 
 MEAN_EXPERT= 10.2
 
+MEAN_BREAK = 60
+
+BREAK_DURATION = 3
+
+SERVICE_RANGE = [50, 90]
+random.seed(RANDOM_SEED)
+
 service_times_front_desk = [] 
 service_times_expert = []
 
 queue_w_times_front_desk = []
 queue_w_times_expert = []
 
+HIGH = 1
+LOW = 2
+
+CUSTOMER_NUMBER = 1000
+
 class Customer(object):
-    def __init__(self, name, env, front_desk_operator, expert_operator):
+
+    total_customer = 0
+
+    def __init__(self, name, env, front_desk_operator, expert_operator, action_break_generator):
         self.env = env
         self.name = name
         self.arrival_to_front_desk = None
         self.arrival_to_expert = None
         self.action = env.process(self.call())
+        self.action_break_generator = action_break_generator
     
     def call(self):
+
+        Customer.total_customer += 1
+        
         print('%s initiated a call at %g' % (self.name, self.env.now))
 
         self.arrival_to_front_desk = self.env.now
@@ -49,7 +67,7 @@ class Customer(object):
 
         self.arrival_to_expert = self.env.now
 
-        with expert_operator.request() as req:
+        with expert_operator.request(priority = HIGH) as req:
             
             patience = random.expovariate(MEAN_RENEGE)
 
@@ -64,6 +82,9 @@ class Customer(object):
                 print('%s is reneged from the queue of the expert operator at %g' % (self.name, self.env.now))
                 env.exit()
 
+        if Customer.total_customer == CUSTOMER_NUMBER and self.action_break_generator.is_alive:
+          self.action_break_generator.interrupt()
+
     def get_front_desk_service(self):
         #duration = numpy.random.lognormal(MEAN_FRONT_DESK, STD_FRONT_DEKS)
         duration = random.expovariate(MEAN_EXPERT)
@@ -75,13 +96,29 @@ class Customer(object):
         yield self.env.timeout(duration)
         service_times_expert.append(duration)
 
-def customer_generator(env, front_desk_operator, expert_operator):
-    for i in range(10):
+def customer_generator(env, front_desk_operator, expert_operator, action_break_generator):
+    for i in range(CUSTOMER_NUMBER):
         yield env.timeout(random.expovariate(INTERARRIVAL_RATE))
-        Customer('Cust %s' % (i+1), env, front_desk_operator, expert_operator)  
+        Customer('Cust %s' % (i+1), env, front_desk_operator, expert_operator, action_break_generator)  
+
+def break_generator(env, front_desk_operator, expert_operator):
+    try:
+        while True:
+            duration = numpy.random.poisson(MEAN_BREAK)
+            yield env.timeout(duration)
+            print("Expert operator has dediced to give break at", env.now)
+            with expert_operator.request(priority = LOW) as req:
+                yield req
+                print("Expert operator has started to the break at", env.now)
+                yield env.timeout(BREAK_DURATION)
+                print("Expert operator has finished the break at", env.now)
+    except simpy.Interrupt:
+        print('Expert will no longer give breaks')
 
 env = simpy.Environment()
 front_desk_operator = simpy.Resource(env, capacity = 1)
-expert_operator = simpy.Resource(env, capacity = 1)
-env.process(customer_generator(env, front_desk_operator, expert_operator))
+expert_operator = simpy.PriorityResource(env, capacity = 1)
+action_break_generator = env.process(break_generator(env, front_desk_operator, expert_operator))
+env.process(customer_generator(env, front_desk_operator, expert_operator, action_break_generator))
+
 env.run() 
